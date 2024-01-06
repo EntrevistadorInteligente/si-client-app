856c3590e2e93f2d61cceb570ab585c446e04ea3
JamiltonQuintero <transportador1118@gmail.com>
2024-01-06 10:36:46 -0500

    Is created Basing architecture on basic angular recommendations

---
+++ src/app/core/components/alert/alert.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert.component.html',
+  styleUrl: './alert.component.scss'
+})
+export class AlertComponent {
+
+}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AlertComponent } from './components/alert/alert.component';
+import { LoaderComponent } from './components/loader/loader.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+  ],
+})
+export class CoreModule { }
