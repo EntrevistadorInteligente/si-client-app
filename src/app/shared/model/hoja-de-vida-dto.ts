8e3308c5df5bdbaf43a64c21da25c4ff1a540b31
Mathias Ledesma <romanmathias21@gmail.com>
2024-04-15 16:35:04 -0300

    se agrego la logica del perfil

---
+++ src/app/shared/model/hoja-de-vida-dto.ts
@@ -0,0 +1,39 @@
+export class HojaDeVidaDto{
+    username: string;
+    nombre: string;
+    perfil: string;
+    seniority: string;
+    tecnologiasPrincipales: Array<string>;
+    experienciasLaborales: Array<string>;
+    habilidadesTecnicas: Array<string>;
+    certificaciones: Array<string>;
+    proyectos: Array<string>;
+    nivelIngles: String;
+    otrasHabilidades: Array<string>;
+
+    constructor(
+        username: string = "",
+        nombre: string = "",
+        perfil: string = "",
+        seniority: string = "",
+        tecnologiasPrincipales: Array<string> = [],
+        experienciasLaborales: Array<string> = [],
+        habilidadesTecnicas: Array<string> = [],
+        certificaciones: Array<string> = [],
+        proyectos: Array<string> = [],
+        nivelIngles: String = "",
+        otrasHabilidades: Array<string> = []
+    ){
+        this.username = username;
+        this.nombre = nombre;
+        this.perfil = perfil;
+        this.seniority = seniority;
+        this.tecnologiasPrincipales = tecnologiasPrincipales;
+        this.experienciasLaborales = experienciasLaborales;
+        this.habilidadesTecnicas = habilidadesTecnicas;
+        this.certificaciones = certificaciones;
+        this.proyectos = proyectos;
+        this.nivelIngles = nivelIngles;
+        this.otrasHabilidades = otrasHabilidades;
+    }
+}
