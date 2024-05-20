c48469ec40479b4360601ff2f97265567a3c4f9e
JamiltonQuintero <transportador1118@gmail.com>
2024-05-20 11:35:25 -0500

    Se repara flujo entrevista con estado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,31 +2,15 @@ export const environment = {
 
   production: true,
 
-  // OAUTH MODULE -- DEL APP MODULE
-  oauthModuleUrl: 'https://gateway.pruebas-entrevistador-inteligente.site/api/*',
-
-  // URL ALERTSERVICE
-  urlOrquestador: 'https://gateway.pruebas-entrevistador-inteligente.site/api/orquestador/v1/eventos/subscribe',
-  urlFeedback: 'https://gateway.pruebas-entrevistador-inteligente.site/api/feedback/v1/eventos/subscribe',
-
-  // URL FEEDBACKSERVICE
-  urlFeedbackService: 'https://gateway.pruebas-entrevistador-inteligente.site/api/feedback/v1/',
-  urlFeedbackMsg: 'https://gateway.pruebas-entrevistador-inteligente.site/api/ms2/',
-
-  // URL INTEGRADORSERVICE
-  urlEntrevistador: 'https://gateway.pruebas-entrevistador-inteligente.site/api/orquestador/v1/entrevistador/',
-  urlMsgEntrevistador: 'https://gateway.pruebas-entrevistador-inteligente.site/api/ms2/',
-
-  // SSE SERVICE
-  sseUrlOrquestador: 'https://gateway.pruebas-entrevistador-inteligente.site/api/orquestador/v1/eventos/subscribe',
-  sseUrlFeedback: 'https://gateway.pruebas-entrevistador-inteligente.site/api/administrador-entrevista/v1/eventos/subscribe',
-
-  // DIRECCI�N IP DEL SERVIDOR
-  SERVER_IP_ADDRESS: '192.168.1.100',
+  oauthModuleUrl: 'http://localhost:8765/api/*',
 
   // INTEGRADOR SERVICE
-  orquestadorURL: 'https://gateway.pruebas-entrevistador-inteligente.site/api/orquestador',
+  orquestadorURL: 'http://localhost:8765/api/orquestador/v1',
 
   // FEEDBACK SERVICE
-  feedbackURL: 'https://gateway.pruebas-entrevistador-inteligente.site/api/administrador-entrevista/v1',
+  feedbackURL: 'http://localhost:8765/api/administrador-entrevista/v1',
+
+  // FEEDBACK SERVICE
+  notifcacionesURL: 'http://localhost:8765/api/notificaciones/v1'
+
 };
