0541e1cbc187d66c3eee2119f337cd8231fa3bf7
DylanSerkovich <n00189659@upn.pe>
2024-07-23 11:02:52 -0500

    Agregar carrusel a preguntas

--- src/app/components/zona-entrevista/multipasos/paso-4/paso-4.component.ts
+++ src/app/components/zona-entrevista/multipasos/paso-4/paso-4.component.ts
@@ -4,97 +4,137 @@ import {
   HostListener,
   Input,
   OnInit,
-  ViewChild,
-} from "@angular/core";
-import { FeedbackService } from "src/app/shared/services/domain/feedback.service";
-import * as data from "../../../../shared/data/animation/ribbons";
-import Swal from "sweetalert2";
-import { IntegradorService } from "src/app/shared/services/domain/integrador.service";
+  QueryList,
+  Renderer2,
+  ViewChildren,
+} from '@angular/core';
+import { FeedbackService } from 'src/app/shared/services/domain/feedback.service';
+import * as data from '../../../../shared/data/animation/ribbons';
+import Swal from 'sweetalert2';
+import { IntegradorService } from 'src/app/shared/services/domain/integrador.service';
+import { FeedbackComentarioDto } from 'src/app/shared/model/feedback-dto';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: "app-paso-4",
-  templateUrl: "./paso-4.component.html",
-  styleUrl: "./paso-4.component.scss",
+  selector: 'app-paso-4',
+  templateUrl: './paso-4.component.html',
+  styleUrl: './paso-4.component.scss',
 })
 export class Paso4Component implements OnInit {
-  @ViewChild("divpregunta") divpregunta!: ElementRef;
-  @ViewChild("pfeedback") pfeedback!: ElementRef;
-
+  @ViewChildren('divpregunta') ribbons: QueryList<ElementRef>;
+  @ViewChildren('pfeedback') feedbacks: QueryList<ElementRef>;
   public ribbon = data.ribbons;
   public ribbonColor = data.ribbonColor;
+  public isCollapsed = true;
+
   @Input() idEntrevista: string;
-  feedbackItems: any[] = [];
-  paginationSide = "center";
+  feedbackItems: FeedbackComentarioDto[] = [];
+  paginationSide = 'center';
   currentIndex: number = 0;
   pageSize: number = 1;
   maxPagesToShow: number = 5;
   isVisibleAnswer: boolean = false;
 
+  optionsCustom: OwlOptions = {
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoWidth: true,
+    autoHeight: true,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  };
+
   constructor(
     private feedbackService: FeedbackService,
-    private integradorService: IntegradorService
+    private integradorService: IntegradorService,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit(): void {
     this.obtenerFeedback(this.idEntrevista);
-    this.adjustMargin();
   }
 
-  get currentFeedback() {
-    return this.feedbackItems.slice(
-      this.currentIndex,
-      this.currentIndex + this.pageSize
-    );
+  animateCarousel(): void {
+    setTimeout(() => {
+      const owlStage = document.querySelector('.owl-stage');
+      if (owlStage) {
+        const parentElement = owlStage.parentElement;
+        if (parentElement) {
+          this.renderer.setStyle(
+            parentElement,
+            'transition',
+            'transform 0.7s ease-in-out'
+          );
+          this.renderer.setStyle(
+            parentElement,
+            'transform',
+            'translate3d(-100px, 0, 0)'
+          );
+
+          setTimeout(() => {
+            this.renderer.setStyle(
+              parentElement,
+              'transform',
+              'translate3d(0, 0, 0)'
+            );
+          }, 400);
+
+          setTimeout(() => {
+            this.renderer.removeStyle(parentElement, 'transform');
+          }, 300); // Additional delay to remove the transform style
+        } else {
+          console.error('Parent of owl-stage not found');
+        }
+      } else {
+        console.error('Owl stage not found');
+      }
+    }, 0); // Adjust this timeout if necessary
   }
 
   obtenerFeedback(entrevistaId: string): void {
     this.feedbackService.obtenerFeedback(entrevistaId).subscribe({
-      next: (feedback) => {
+      next: (feedback: FeedbackComentarioDto[]) => {
         this.feedbackItems = feedback;
+        console.log('feedback', this.feedbackItems);
+        this.adjustMargin();
+        this.animateCarousel();
       },
-      error: (error) => {
+      error: error => {
         console.error(error);
       },
     });
   }
 
-  changePage(index: number): void {
-    if (index >= 0 && index < this.totalPages) {
-      this.currentIndex = index;
-    }
-    this.adjustMargin();
-  }
+  ngAfterViewInit() {}
 
-  get totalPages(): number {
-    return Math.ceil(this.feedbackItems.length / this.pageSize);
-  }
-
-  get pages(): number[] {
-    const half = Math.floor(this.maxPagesToShow / 2);
-    let start = Math.max(0, this.currentIndex - half);
-    let end = Math.min(this.totalPages, start + this.maxPagesToShow);
-
-    if (end - start < this.maxPagesToShow) {
-      start = Math.max(0, end - this.maxPagesToShow);
-    }
-
-    return Array.from({ length: end - start }, (_, i) => start + i + 1);
-  }
-
-  @HostListener("window:resize")
+  @HostListener('window:resize')
   onResize() {
     this.adjustMargin();
   }
 
   private adjustMargin() {
     setTimeout(() => {
-      const containerHeight = this.divpregunta.nativeElement.offsetHeight;
       const screenWidth = window.innerWidth;
 
-      this.pfeedback.nativeElement.style.marginTop =
-        screenWidth <= 400
-          ? `${containerHeight / 2.7}%`
-          : `${containerHeight / 2}px`;
+      this.ribbons.forEach((divpregunta, index) => {
+        const containerHeight = divpregunta.nativeElement.offsetHeight;
+        const pfeedback = this.feedbacks.toArray()[index];
+
+        if (pfeedback) {
+          pfeedback.nativeElement.style.marginTop = `${containerHeight}px`;
+        }
+      });
     }, 400);
   }
 
@@ -105,50 +145,50 @@ export class Paso4Component implements OnInit {
   withFeedback() {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
-        confirmButton: "btn btn-success",
-        cancelButton: "btn btn-danger",
-        input: "placeholder-color",
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+        input: 'placeholder-color',
       },
       buttonsStyling: false,
     });
 
     swalWithBootstrapButtons
       .fire({
-        title: "Terminar entrevista",
-        text: "¡Una vez terminada, no podrás ver el feedback tu entrevista en proceso nuevamente e iniciarás una nueva entrevista!",
-        input: "textarea",
+        title: 'Terminar entrevista',
+        text: '¡Una vez terminada, no podrás ver el feedback tu entrevista en proceso nuevamente e iniciarás una nueva entrevista!',
+        input: 'textarea',
         inputPlaceholder:
-          "Por favor danos tu feedback sobre el proceso de preparación de la entrevista, esto nos ayudará a mejorar nuestra aplicación. Tu opinión es muy importante para nosotros (opcional)",
+          'Por favor danos tu feedback sobre el proceso de preparación de la entrevista, esto nos ayudará a mejorar nuestra aplicación. Tu opinión es muy importante para nosotros (opcional)',
         showCancelButton: true,
-        confirmButtonText: "Terminar entrevista",
-        cancelButtonText: "Cancelar",
+        confirmButtonText: 'Terminar entrevista',
+        cancelButtonText: 'Cancelar',
         reverseButtons: true,
       })
       .then((result: any) => {
         if (result.isConfirmed) {
-          const feedback = result.value || "";
+          const feedback = result.value || '';
           this.integradorService
             .terminarEntrevistaEnCurso(this.idEntrevista, feedback)
             .subscribe({
-              next: (response) => {
+              next: response => {
                 if (response) {
                   swalWithBootstrapButtons
                     .fire(
-                      "Entrevista terminada!",
-                      "Gracias por tu feedback. Ahora puedes generar una nueva entrevista.",
-                      "success"
+                      'Entrevista terminada!',
+                      'Gracias por tu feedback. Ahora puedes generar una nueva entrevista.',
+                      'success'
                     )
                     .then(() => {
                       window.location.reload();
                     });
                 }
               },
-              error: (error) => {
+              error: error => {
                 console.error(error);
                 swalWithBootstrapButtons.fire(
-                  "Error",
-                  "Hubo un problema al terminar la entrevista.",
-                  "error"
+                  'Error',
+                  'Hubo un problema al terminar la entrevista.',
+                  'error'
                 );
               },
             });
