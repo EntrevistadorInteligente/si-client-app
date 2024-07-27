643943c68d8ad7c640d5f2cadcf7c49c76750531
DylanSerkovich <n00189659@upn.pe>
2024-07-27 17:41:43 -0500

    comentarios resueltos

--- src/app/components/zona-entrevista/multipasos/paso-4/paso-4.component.ts
+++ src/app/components/zona-entrevista/multipasos/paso-4/paso-4.component.ts
@@ -1,19 +1,9 @@
-import {
-  Component,
-  ElementRef,
-  Input,
-  OnInit,
-  QueryList,
-  Renderer2,
-  ViewChild,
-  ViewChildren,
-} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { FeedbackService } from 'src/app/shared/services/domain/feedback.service';
 import Swal from 'sweetalert2';
 import { IntegradorService } from 'src/app/shared/services/domain/integrador.service';
 import { FeedbackComentarioDto } from 'src/app/shared/model/feedback-dto';
-import { CarouselComponent, OwlOptions } from 'ngx-owl-carousel-o';
-import { CarouselService } from 'ngx-owl-carousel-o/lib/services/carousel.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-paso-4',
@@ -22,12 +12,6 @@ import { CarouselService } from 'ngx-owl-carousel-o/lib/services/carousel.servic
 })
 export class Paso4Component implements OnInit {
   public isCollapsed = true;
-  // @ViewChildren('carousel')
-  // public carousel: QueryList<CarouselComponent>;
-  // @ViewChild('carouselContainer')
-  // public carouselContainer: ElementRef;
-
-  // private resizeObserver: ResizeObserver;
 
   @Input() idEntrevista: string;
   feedbackItems: FeedbackComentarioDto[] = [];
@@ -41,8 +25,8 @@ export class Paso4Component implements OnInit {
     stagePadding: 50,
     nav: true,
     navText: [
-      "<i class='icofont icofont-curved-left'></i>",
-      "<i class='icofont icofont-curved-right'></i>",
+      `<i class='icofont icofont-curved-left'></i>`,
+      `<i class='icofont icofont-curved-right'></i>`,
     ],
     responsive: {
       0: {
@@ -53,93 +37,23 @@ export class Paso4Component implements OnInit {
 
   constructor(
     private feedbackService: FeedbackService,
-    private integradorService: IntegradorService,
-    private renderer: Renderer2
+    private integradorService: IntegradorService
   ) {}
 
   ngOnInit(): void {
     this.obtenerFeedback(this.idEntrevista);
   }
 
-  animateCarousel(): void {
-    setTimeout(() => {
-      const owlStage = document.querySelector('.owl-stage');
-      if (owlStage) {
-        const parentElement = owlStage.parentElement;
-        if (parentElement) {
-          this.renderer.setStyle(
-            parentElement,
-            'transition',
-            'transform 0.7s ease-in-out'
-          );
-          this.renderer.setStyle(
-            parentElement,
-            'transform',
-            'translate3d(-100px, 0, 0)'
-          );
-
-          setTimeout(() => {
-            this.renderer.setStyle(
-              parentElement,
-              'transform',
-              'translate3d(0, 0, 0)'
-            );
-          }, 400);
-
-          setTimeout(() => {
-            this.renderer.removeStyle(parentElement, 'transform');
-          }, 300); // Additional delay to remove the transform style
-        } else {
-          console.error('Parent of owl-stage not found');
-        }
-      } else {
-        console.error('Owl stage not found');
-      }
-    }, 0); // Adjust this timeout if necessary
-  }
-
   obtenerFeedback(entrevistaId: string): void {
     this.feedbackService.obtenerFeedback(entrevistaId).subscribe({
       next: (feedback: FeedbackComentarioDto[]) => {
         this.feedbackItems = feedback;
-        console.log('feedback', this.feedbackItems);
-        //this.animateCarousel();
       },
       error: error => {
         console.error(error);
       },
     });
   }
-
-  // ngAfterViewInit(): void {
-  //   this.resizeObserver = new ResizeObserver(entries => {
-  //     for (let entry of entries) {
-  //       if (entry.target === this.carouselContainer.nativeElement) {
-  //         this.refreshCarousel();
-  //       }
-  //     }
-  //   });
-
-  //   this.resizeObserver.observe(this.carouselContainer.nativeElement);
-  // }
-
-  // ngOnDestroy(): void {
-  //   if (this.resizeObserver) {
-  //     this.resizeObserver.disconnect();
-  //   }
-  // }
-
-  // refreshCarousel(): void {
-  //   setTimeout(() => {
-  //     this.carousel.forEach(c => {
-  //       const anyService = c as any;
-  //       const carouselService = anyService.carouselService as CarouselService;
-  //       console.log('resize owl');
-  //       carouselService.refresh();
-  //     });
-  //   }, 200);
-  //   console.log('resize');
-  // }
 
   withFeedback() {
     const swalWithBootstrapButtons = Swal.mixin({
